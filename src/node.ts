a6cf81715ea7eeee382ca7d42a998c5370d0621f
Rahul Ghangas <rahulghangas0@gmail.com>
2022-12-03 17:16:20 +0530

    feat: add proof validation

--- src/node.ts
+++ src/node.ts
@@ -1,6 +1,6 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import { Request, Response } from 'express';
 import { validate } from 'class-validator';
 import { plainToInstance } from 'class-transformer';
@@ -8,6 +8,8 @@ import { CreateShare, GetShare } from './request-types';
 import * as db from './utils/kvwrapper';
 import { sleep } from './utils/helpers';
 import * as fs from 'fs';
+import { PROJECT_DIR } from '../settings';
+import path from 'path';
 
 const snarkjs = require('snarkjs');
 
@@ -17,38 +19,47 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/createshare', async (req: Request, res: Response) => {
-  const user = plainToInstance(CreateShare, req.body);
-  const errors = await validate(user);
+  const createShare = plainToInstance(CreateShare, req.body as unknown);
+  const errors = await validate(createShare);
   if (errors.length > 0) {
     res.status(400).send(errors);
+  } else {
+    const key = createShare.address;
+    const value = JSON.stringify({hash: createShare.hash, share: createShare.share});
+    await db.addOrUpdateKeyValue(key, value);
+    res.send('share submitted succesfully!');
   }
-
-  const key = user.address;
-  const value = JSON.stringify({hash: user.hash, share: user.share});
-  await db.addOrUpdateKeyValue(key, value);
-  res.send('share submitted succesfully!');
   
 });
 
 app.get('/getshare', async (req: Request, res: Response) => {
-    const proof = plainToInstance(GetShare, req.body);
+    const proof = plainToInstance(GetShare, req.body as unknown);
     const errors = await validate(proof);
     if (errors.length > 0) {
         res.status(400).send(errors);
-    }
+    } else {
 
-    const vKey = JSON.parse(fs.readFileSync("verification_key.json").toString());
-    const verificationResult = await snarkjs.groth16.verify(vKey, proof.public_signals, proof.proof);
-
-    if (verificationResult === true) {
-        const key = proof.address;
-        let value!: string;
+        const vKey = JSON.parse(fs.readFileSync(path.join(PROJECT_DIR, "recovery-circom/circuits", "verification_key.json")).toString());
+        
+        let verificationResult = true;
         try {
-            value = await db.getValueFromKey(key);
-            res.send(value);
-        } catch(e) {
-            res.status(400).send('share not found');
-        } 
+            verificationResult = await snarkjs.groth16.verify(vKey, proof.public_signals, proof.proof);
+        } catch(err) {
+            console.log(err);
+            res.status(400).send("invalid proof");
+        }
+
+        if (verificationResult === true) {
+            const key = proof.address;
+            let value!: string;
+            try {
+                value = await db.getValueFromKey(key);
+                const valueObject: {hash: string, share: string} = JSON.parse(value);
+                res.send(valueObject.share);
+            } catch(e) {
+                res.status(400).send('share not found');
+            } 
+        }
     }
 });
 
