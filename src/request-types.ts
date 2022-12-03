884ccda1325f7cae59feaef713d3e89b4c0d5782
Rahul Ghangas <rahulghangas0@gmail.com>
2022-12-03 14:17:05 +0530

    feat: define REST api types

---
+++ src/request-types.ts
@@ -0,0 +1,19 @@
+export class CreateShare {
+    addres: string;
+    hash: string;
+    share: string;
+}
+
+export class GetShare {
+    address: string;
+
+    proof: {
+        pi_a: any[];
+        pi_b: any[][];
+        pi_c: any[];
+        protocol: any;
+        curve: any;
+    }
+
+    public_signals: string[];
+}
