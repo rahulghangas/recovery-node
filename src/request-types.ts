57e5c68bf4835848a80feeb1d5ad733009019958
Rahul Ghangas <rahulghangas0@gmail.com>
2022-12-03 17:16:50 +0530

    chore: add strict validation constaints to request types

--- src/request-types.ts
+++ src/request-types.ts
@@ -1,7 +1,20 @@
+import { IsString } from "class-validator";
+
 export class CreateShare {
+    @IsString()
     address: string;
+
+    @IsString()
     hash: string;
+
+    @IsString()
     share: string;
+
+    constructor(address: string, hash: string, share: string) {
+        this.address = address;
+        this.hash = hash;
+        this.share = share;
+    }
 }
 
 export class GetShare {
@@ -16,4 +29,10 @@ export class GetShare {
     }
 
     public_signals: any[];
+
+    constructor(address: string, proof: any, public_signals: any[]) {
+        this.address = address;
+        this.proof = proof;
+        this.public_signals = public_signals;
+    }
 }
