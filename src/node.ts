15165fbf7029be1ec45ac8121699dae0972e814c
Rahul Ghangas <rahulghangas0@gmail.com>
2022-12-03 14:17:25 +0530

    feat: add validation logic for type assertion of body

--- src/node.ts
+++ src/node.ts
@@ -1,20 +1,38 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import { Request, Response } from 'express';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { CreateShare, GetShare } from './request-types';
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get('/', (req: any, res: any) => {
-    res.send('Hello World!');
+app.post('/createshare', async (req: Request, res: Response) => {
+  const user = plainToInstance(CreateShare, req.body);
+  const errors = await validate(user);
+  if (errors.length > 0) {
+    res.status(400).send(errors);
+  } else {
+    res.send('User successfully created!');
+  }
+
+  
 });
 
-app.post('/', (req: any, res: any) => {
-    console.log(req.body);
-    res.send('Hello World!');
+app.get('/getshare', async (req: Request, res: Response) => {
+    const user = plainToInstance(GetShare, req.body);
+    const errors = await validate(user);
+    if (errors.length > 0) {
+        res.status(400).send(errors);
+    } else {
+        res.send('User successfully created!');
+    }
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+  console.log('Server started on port 3000!');
 });
